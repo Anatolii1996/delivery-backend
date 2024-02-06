c4d0bf2d381eff280652518b339357cf4099cb47
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2024-02-06 09:18:18 +0200

    conected to mongo

---
+++ api.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const PORT = 3002;
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+// app.use(visitRouter, commentRouter,blockedRouter)
+
+mongoose
+    .connect(process.env.URL)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log(`DB connection error: ${err}`));
+
+
+
+app.listen(PORT, (err) => {
+    err ? console.log(err) : console.log(`Listening port ${PORT}`);
+});
