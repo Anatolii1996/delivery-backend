3f01d6b315eb2df0683391232db54a59c175b6a4
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2024-03-03 21:10:44 +0200

    removing deprecated orders

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -1,8 +1,22 @@
 const Order = require("../models/order");
+const moment = require("moment");
 const { handleError } = require("../helper")
 
+const removeDepricatedOrder = async () => {
+    try {
+        const currentDate = moment().add(120, 'minutes').format("DD.MM.YYYY");
+
+        await Order.deleteMany({ date: { $lt: currentDate } });
+
+        console.log("Устаревшие заказы успешно удалены.");
+    } catch (err) {
+        handleError(res, err);
+    }
+};
 const setOrder = async (req, res) => {
     // console.log(req.body)
+    removeDepricatedOrder()
+
     const order = new Order(req.body);
     // // console.log(visit)
     await order
--- models/order.js
+++ models/order.js
@@ -1,24 +1,29 @@
 const mongoose = require("mongoose");
+const moment = require("moment");
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-    tel: String,
-    address: String,
-    price: Number,
-    details: {
-      Меню1: {
-        count: Number,
-        dishes:[String]
-      },
-      Меню2: {
-        count: Number,
-        dishes:[String],
-      },
-      Десерт: {
-        count: Number,
-        dishes:[String]
-      }
+  tel: String,
+  address: String,
+  price: Number,
+  date: {
+    type: String,
+    default: moment().add(120, 'minutes').format("DD.MM.YYYY")
+  },
+  details: {
+    Меню1: {
+      count: Number,
+      dishes: [String]
+    },
+    Меню2: {
+      count: Number,
+      dishes: [String],
+    },
+    Десерт: {
+      count: Number,
+      dishes: [String]
     }
+  }
 });
 
 const Order = mongoose.model("Order", orderSchema);
