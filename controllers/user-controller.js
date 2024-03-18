87aa773814b1680da138fcdf8661fcc174c37779
Anatolii Tkachenko <anatoly.tka4enko2014@gmail.com>
2024-03-18 16:22:37 +0200

    added activation link

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,20 +1,23 @@
 const User = require("../models/user");
 const { handleError } = require("../helper")
 const bcrypt = require("bcrypt");
+const uuid = require("uuid")
 
 const createUser = async (req, res) => {
     console.log(req.body)
-    const candidate = await User.findOne({email: req.body.email});
-    if (candidate){
+    const candidate = await User.findOne({ email: req.body.email });
+    if (candidate) {
         throw new Error("Користувач з такою адресою вже зарєестрований")
     }
-    const hashPassword=await bcrypt.hash(req.body.password, 3);
+    const hashPassword = await bcrypt.hash(req.body.password, 3);
+    const activationLink = uuid.v4()
     const user = new User({
-        name:req.body.name,
-        email:req.body.email,
-        password:hashPassword,
-        tel:req.body.tel,
-        address:req.body.address
+        name: req.body.name,
+        email: req.body.email,
+        password: hashPassword,
+        tel: req.body.tel,
+        address: req.body.address,
+        activationLink: activationLink
     });
     // console.log(visit)
     await user
@@ -32,52 +35,52 @@ const createUser = async (req, res) => {
 
 const login = async (req, res, next) => {
     // console.log(req.body)
-    try{
-        
-    }catch(err){
+    try {
+
+    } catch (err) {
 
     }
-  
+
 };
 
 const logout = async (req, res, next) => {
     // console.log(req.body)
-    try{
-        
-    }catch(err){
-        
+    try {
+
+    } catch (err) {
+
     }
-  
+
 };
 
 const activate = async (req, res, next) => {
     // console.log(req.body)
-    try{
-        
-    }catch(err){
-        
+    try {
+
+    } catch (err) {
+
     }
-  
+
 };
 const refresh = async (req, res, next) => {
     // console.log(req.body)
-    try{
-        
-    }catch(err){
-        
+    try {
+
+    } catch (err) {
+
     }
-  
+
 };
 const getUsers = async (req, res, next) => {
     // console.log(req.body)
-    try{
-        res.json(["123","456"])
-    }catch(err){
-        
+    try {
+        res.json(["123", "456"])
+    } catch (err) {
+
     }
-  
+
 };
 
 
 
-module.exports = { createUser, login, logout, activate, refresh , getUsers};
+module.exports = { createUser, login, logout, activate, refresh, getUsers };
